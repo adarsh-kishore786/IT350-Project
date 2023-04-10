57dbb0bbc086ebd736051f98b43bc836e8a034de
Adarsh Kishore <adarsh.kishore786@gmail.com>
2023-04-10 11:09:52 +0530

    Got DA data

--- Display/comments_tester.py
+++ Display/comments_tester.py
@@ -1,27 +1,31 @@
-from utility import CommentScraper
 from instagrapi import Client
 from dotenv import load_dotenv  
 import os
 
-def main():
+load_dotenv()
+
+def get_comments(url):
   load_dotenv()
   baseUrl = "https://www.instagram.com/"
-  url = "p/CjI2UgUJZMBj3JqdXLDosPYqyx_Mw742hKEPBc0/"
   
   INSTA_USERNAME = os.environ.get('INSTA_USERNAME')
   INSTA_PASSWORD = os.environ.get('INSTA_PASSWORD')
   
-  # scraper = CommentScraper(baseUrl, url)
-  # print(scraper.soup.find("title"))
   cl = Client()
   cl.login(INSTA_USERNAME, INSTA_PASSWORD)
   
   media_id = cl.media_id(cl.media_pk_from_url(baseUrl+url))
   
-  comments = cl.media_comments(media_id)
+  return cl.media_comments(media_id, 20)
+
+def main():
+  comments = get_comments("p/Cqzy-IOrTyD")
+  # scraper = CommentScraper(baseUrl, url)
+  # print(scraper.soup.find("title"))
   
   for c in comments:
-    print(type(c))
+    print(vars(c)["text"])
+    print()
 
 if __name__ == "__main__":
   main()
