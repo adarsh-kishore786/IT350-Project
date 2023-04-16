768d5cdb0de50299a9155dc15d119cc235e062ca
Ranjana Kambhammettu <ranjanak207@gmail.com>
2023-04-16 23:51:08 +0530

    Scrape Twitter for all content related to a query

---
+++ Display/comments_twitter.py
@@ -0,0 +1,20 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# Creating list to append tweet data to
+tweets_list2 = []
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('महाराष्ट्र lang:hi since:2023-02-01 until:2020-03-31').get_items()):
+    if i>10:
+        break
+    tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+    
+# Creating a dataframe from the tweets list above
+tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+print(tweets_df2)
+
+# query = 'lang:ur' #ur is code for urdu
+# #limit = 10
+# urduTweets = sntwitter.TwitterSearchScraper(query).get_items()
+# print(urduTweets)
