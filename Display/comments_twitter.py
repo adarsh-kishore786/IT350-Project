d447022c4065be3bfc77285f386b6572daa00f60
Ranjana Kambhammettu <ranjanak207@gmail.com>
2023-04-17 01:22:48 +0530

    Changed Telugu URL and edited code for Twitter

--- Display/comments_twitter.py
+++ Display/comments_twitter.py
@@ -5,14 +5,15 @@ import pandas as pd
 tweets_list2 = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('महाराष्ट्र lang:hi since:2023-02-01 until:2020-03-31').get_items()):
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:aajtak पुलिस lang:hi since:2023-01-01 until:2023-03-31 include:replies').get_items()):
     if i>10:
         break
     tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
     
 # Creating a dataframe from the tweets list above
 tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
-print(tweets_df2)
+tweets_df2.to_csv('tweets.csv')
+print(tweets_df2['Text'])
 
 # query = 'lang:ur' #ur is code for urdu
 # #limit = 10
